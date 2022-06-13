13262a0cfa978b17ebb4b1bc69de7edc14666cc4
Ted Enston <edwardenston@gmail.com>
2022-06-13 18:16:08 +0100

    2022.06.13.17.15

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-import pyperclip
 import datetime
+
+import pyperclip
 
 print('Time to get the time')
 
 now = datetime.datetime.utcnow()
-nowformated = now.strftime("%Y.%m.%d.%H.%M")
+nowFormatted = now.strftime("%Y.%m.%d.%H.%M")
 
 # Print the current time in UTC format to the console
-print(nowformated)
+print(nowFormatted)
 
 print('The time will be copied to your clipboard')
 
 # Copy the current time to the clipboard
-pyperclip.copy(nowformated)
+pyperclip.copy(nowFormatted)
